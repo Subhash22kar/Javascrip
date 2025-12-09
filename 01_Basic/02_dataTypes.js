b83281c65d15e337953fb87e0d5fc13149c2c645
Subhash22kar <0126it201107@oriental.ac.in>
2025-12-09 19:58:41 +0000

    Data Types

---
+++ 01_Basic/02_dataTypes.js
@@ -0,0 +1,26 @@
+"use strict";// Treat all JS code as a newer version
+
+//alert( 3 + 3);// we are using nodeJs, not a browser
+
+console.log(3 + 
+    3) // code readability should be high
+
+    console.log("Subhash")
+
+    let name = "subhash"
+    let age = 26
+    let isLoggedIn = false
+    let state;
+
+    // number => 2 to power 53
+    // bigint
+    // string => ""
+    // boolean => true/false
+    // null => standalone value
+    // undefined =>
+    // symbol => unique
+    
+    // object
+
+    console.log(typeof undefined); // undefined
+    console.log(typeof null); // object
