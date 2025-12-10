900bde662ab3357fa4377120b318899bfcc3824d
Subhash22kar <0126it201107@oriental.ac.in>
2025-12-10 15:55:42 +0000

    Data Types Summary

---
+++ 01_Basic/dataTypes_Summary.js
@@ -0,0 +1,46 @@
+// Primitive
+
+// 7 Types : String, Number, Boolean, Null , undefinded , Symbol, BigInt
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const  anotherId = Symbol('123')
+
+console.log(id === anotherId);
+
+const bigNumber = 123431223445n;
+
+console.log(bigNumber);
+
+const heros = ["shaktiman", "doremon", "ritesh"]
+
+ let myObj = {
+    name: "hitesh",
+    age: 22,
+}
+
+const myFunction = function(){
+    console.log("Hello world");
+}
+
+console.log(typeof bigNumber);
+
+console.log(typeof outsideTemp );
+
+console.log(typeof scoreValue);
+
+console.log(typeof anotherId);
+console.log(typeof myFunction);
+console.log(typeof heros);
+console.log(typeof userEmail);
+
+
+// Reference (Non Primitive)
+
+// Array, Objects, Functions
