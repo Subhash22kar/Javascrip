db4fca4e71df77d887c50d30cf545307a7859b66
Subhash22kar <0126it201107@oriental.ac.in>
2025-12-12 10:58:53 +0000

    Object in JavaScript Part 1

--- 02_basics/03_objects.js
+++ 02_basics/03_objects.js
@@ -3,10 +3,13 @@
 
 // Object literals
 
+const mySym = Symbol("key1")
+
 const JsUser = {
 
     name: "Subhash",
     "full name": "Subhash Baviskar",
+    [mySym]: "mykey1",
     age:  18,
     location: "Jaipur",
     email: "subhash@google.com",
@@ -14,6 +17,25 @@ const JsUser = {
     lastLoginDays: ["Monday", "Saturday"]
 }
 
-console.log(JsUser.email);
-console.log(JsUser["email"]);
-console.log(JsUser["full name"]);
+// console.log(JsUser.email);
+// console.log(JsUser["email"]);
+// console.log(JsUser["full name"]);
+// console.log(JsUser[mySym]);
+// console.log(typeof mySym)
+
+JsUser.email = "subhash@chatgpt.com"
+// Object.freeze(JsUser)
+JsUser.email = "subhash@microsoft.com"
+// console.log(JsUser);
+
+JsUser.greeting = function(){
+    console.log("Hello JS User");
+}
+
+JsUser.greetingTwo = function(){
+    console.log(`Hello JS User, ${this.name}`);
+}
+
+
+console.log(JsUser.greeting());
+console.log(JsUser.greetingTwo());
