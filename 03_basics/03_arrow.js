b88be1894d403384a91076bf82d7692a6efdb4ee
Subhash22kar <0126it201107@oriental.ac.in>
2025-12-13 16:52:44 +0000

    Arrow Function and this

---
+++ 03_basics/03_arrow.js
@@ -0,0 +1,39 @@
+const  user = {
+    username : "subhash",
+    price: 999,
+
+    welcomeMessage: function() {
+        console.log(`${this.username}, welcome to website`);
+        console.log(this);
+    }
+}
+
+// user.welcomeMessage()
+// user.username = "Sam"
+// user.welcomeMessage()
+
+// console.log(this);
+
+// function chai(){
+//     console.log(this.username);
+// }
+
+// chai()
+
+// const chai =  function(){
+//     let username = "Subhash"
+//     console.log(this.username);
+// }
+
+const chai = () => {
+    let username = "Subhash"
+    console.log(this);
+}
+
+// chai()
+
+const addTwo = (num1, num2) => {
+  return num1 + num2
+}
+
+console.log(addTwo(3, 4));
