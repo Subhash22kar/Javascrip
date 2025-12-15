7c21447aeb2e7bfcf5962b235be9d9e286e9fedf
Subhash22kar <0126it201107@oriental.ac.in>
2025-12-15 18:00:44 +0000

    Control Flow in JavaScript

--- 04_control_flow/one.js
+++ 04_control_flow/one.js
@@ -25,6 +25,39 @@ const temperature = 41
 
 // console.log(`user power: ${power}`);
 
-const balance = 1000
+// const balance = 1000
 
 // if(balance > 500) console.log("test") , console.log("test2");
+
+// if(balance < 500){
+//     console.log("less than 500");
+
+// }else if(balance < 750){
+//    console.log("less than 750");
+
+// } else if(balance < 900){
+//    console.log("less than 900");
+
+// } else {
+//   console.log("less than 1200");
+// }
+
+const userloggedIn = true
+
+const debitCard = true
+
+const loggedInFromGoogle = false
+
+const loggedInFromEmail = true
+
+if(userloggedIn && debitCard && 2 == 2){
+
+    console.log("Allow  to buy  course");
+
+}
+
+if(loggedInFromGoogle || loggedInFromEmail){
+
+    console.log("User logged in ");
+
+}
