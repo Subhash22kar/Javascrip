4e3d2bc9226ba1140236228b2f8c1ac255de100a
Subhash22kar <0126it201107@oriental.ac.in>
2025-12-17 12:42:12 +0000

    For in loop

---
+++ 05_iterations/four.js
@@ -0,0 +1,37 @@
+const myObject = {
+    js: 'javasript',
+    cpp: "C++",
+    rb: "ruby",
+    swift: " swift by app"
+}
+
+for (const key in myObject) {
+
+    // console.log(`${key} shortcut is  for ${myObject[key]}`);
+    
+    
+}
+
+const programming = ["js", "rb", "py", "java", "cpp"]
+
+for (const key in programming) {
+//   console.log(programming[key]);
+  
+    
+    
+}
+
+
+// const map = new Map()
+// map.set('IN', "India")
+// map.set('IN', "India")
+// map.set('USA', "United State of  America")
+// map.set('Fr', "France")
+
+
+// for (const key in map) {
+    
+//     console.log(key);
+    
+// }
+
